3a76fa98de5a4ad2a3a195e727aa6d5fe37d1bcf
Jhonathan S Soares <jx@vortx.com.br>
2020-04-15 15:29:17 -0300

    add more patterns to test

--- test/validators/format.js
+++ test/validators/format.js
@@ -7,7 +7,6 @@ describe("format validation", () => {
 
         //zipcode regex
         var pattern = /^[0-9]{8}$/
-        
         const samples = [
             {},
             [],
@@ -26,63 +25,94 @@ describe("format validation", () => {
         }
     })
 
-    it("allows values that matches the pattern", function() {
+    it("allows values that matches the pattern", function () {
 
-       //zipcode regex
-       var pattern = /^[0-9]{8}$/
-        
-       const samples = [
-          "05541030",37130000,['37130000']
-       ]
+        const samples = [
+            [new RegExp('^[0-9]{8}$'), '05541030'],
+            [/^[0-9]{8}$/, 26130014],
+            [/^[0-9]{8}$/, ['37130000']],
+            [/\S+@\S+\.\S+/i, 'sample@email.com'],
+            [/^\(?[\d]{3}\)?[\s-]?[\d]{3}[\s-]?[\d]{4}$/, '573-884-1234'],
+        ]
 
-       for (const value of samples) {
-           // given
-           const validations = { format: pattern }
-           // when
-           const ret = validate(value, validations)
-           // then
-           assert.deepStrictEqual(ret, { value: value, errors: [] })
-       }
+        for (const value of samples) {
+            // given
+            const validations = { format: value[0] }
+            // when
+            const ret = validate(value[1], validations)
+            // then
+            assert.deepStrictEqual(ret, { value: value[1], errors: [] })
+        }
 
-      })
+    })
 
+    it('does not allow values that not matches the pattern', () => {
+       
+        const samples = [
+            [new RegExp('^[0-9]{8}$'), 'f05541030'],
+            [/^[0-9]{8}$/, true],
+            [/^[0-9]{8}$/, new Date()],
+            [/\S+@\S+\.\S+/i, 'sampleemail.com'],
+            [/^\(?[\d]{3}\)?[\s-]?[\d]{3}[\s-]?[\d]{4}$/, '73-884-1234'],
+        ]
 
-      it("allows RegExp flag pattern", function() {
+        for (const value of samples) {
+            // given
+            const validations = { format: value[0] }
+            // when
+            const ret = validate(value[1], validations)
+            // then
+            assert.deepStrictEqual(ret, { value: value[1], errors: [{ [err.invalidFormat]: true }] })
+        }
+
+    })
+
+    it("allows expressions that uses flags pattern", function () {
 
         //zipcode regex
-        var pattern = new RegExp('^[0-9]{8}$','i')
-         
-        const samples = [
-           "05541030",37130000
-        ]
- 
-        for (const value of samples) {
-            // given
-            const validations = { format: pattern }
-            // when
-            const ret = validate(value, validations)
-            // then
-            assert.deepStrictEqual(ret, { value: value, errors: [] })
-        }
- 
-       })
+        var pattern = new RegExp('^[0-9]{8}$', 'i')
 
-    
-    it('does not allow values that not matches the pattern', () => {
+        const value = "05541030"
+        // given
+        const validations = { format: pattern }
+        // when
+        const ret = validate(value, validations)
+        // then
+        assert.deepStrictEqual(ret, { value: value, errors: [] })
 
-         //zipcode regex
-         var pattern = /^[0-9]{8}$/
-        
-         const samples = [
-            "fz055410",true,37130.000,new Date()
-         ]
-        for (const value of samples) {
-            // given
-            const validations = { format: pattern  }
-            // when
-            const ret = validate(value, validations)
-            // then
-            assert.deepStrictEqual(ret, { value: value, errors: [{ [err.invalidFormat]: true }] })
-        }
-    })   
+    })
+
+    it("doesn't allow partial matches", function () {
+
+        //zipcode regex
+        var pattern = new RegExp('\\.png$', 'g')
+
+        const value = "foo.png"
+        // given
+        const validations = { format: pattern }
+        // when
+        const ret = validate(value, validations)
+        // then
+        assert.deepStrictEqual(ret, { value: value, errors: [] })
+
+    })
+
+
+
+    it("allows work with multiple validations", function () {
+
+        //zipcode regex
+        var pattern = /^[0-9]{8}$/
+
+        const value = "05541030"
+        // given
+        const validations = { format: pattern, presence: true }
+        // when
+        const ret = validate(value, validations)
+        // then
+        assert.deepStrictEqual(ret, { value: value, errors: [] })
+
+    })
+
+
 })
